ceaf61e3803f7cadf9f47d1943ab9f94ed8e882f
shreya144167 <83177574+shreya144167@users.noreply.github.com>
2024-05-06 15:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,25 +13,23 @@ uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
 def plot_graph(data):
     # Read Excel file into a DataFrame
     df = pd.read_excel(data)
-   
     # Display the DataFrame
     st.write("Data from Excel file:")
     st.write(df)
-   
     # Get column names
     column_names = list(df.columns)
-   
     # Select the first two columns for plotting
     x_axis = "Project Name"
     y_axis = st.selectbox("Select Y-axis", column_names[1:])
-   
     # Plot the graph using seaborn
     st.write("Graph:")
     sns.set_style("whitegrid")
-    sns.barplot(x=x_axis, y=y_axis, data=df)
+    ax = sns.barplot(x=x_axis, y=y_axis, data=df)
+    ax.set_ylim(0, 10)  
     plt.xticks(rotation=90)
     st.pyplot()
-    showPyplotGlobalUse = "false"
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+ 
  
 # Check if file is uploaded
 if uploaded_file is not None:
